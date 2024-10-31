68462b55c3c7818f446a6d75f8ed222f9a6c9d56
ThatOneGuy63 <Seby@algirdo.com>
2024-10-31 19:04:35 -0400

    migrate error fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,31 @@
-import os
 from flask import Flask
 from config import Config
 from .models import db
 from .scheduler import init_scheduler
-from flask_migrate import Migrate, upgrade, init
+from flask_migrate import Migrate
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate = Migrate(app, db)
+    Migrate(app, db)
 
     with app.app_context():
+        from flask_migrate import upgrade, init, migrate
+        import os
+
         if not os.path.exists(os.path.join(os.getcwd(), 'migrations')):
             init()
+
+        try:
             migrate(message="Initial migration")
             upgrade()
-
+        except Exception as e:
+            print(f"Error applying migrations: {e}")
+            
         init_scheduler(app)
-
+        
     from .routes import main_bp
     app.register_blueprint(main_bp)
 
