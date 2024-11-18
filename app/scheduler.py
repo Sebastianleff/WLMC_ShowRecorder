d90d2f89932a06995e93d07d5126e29e16aa1f38
ThatOneGuy63 <Seby@algirdo.com>
2024-11-18 16:30:28 -0500

    refactor: standardize indentation and improve error logging in scheduler

--- app/scheduler.py
+++ app/scheduler.py
@@ -12,41 +12,42 @@ os.makedirs(Config.OUTPUT_FOLDER, exist_ok=True)
 scheduler = BackgroundScheduler()
 
 def init_scheduler(app):
-	"""Initialize and start the scheduler with the Flask app context."""
-	scheduler.start()
-	with app.app_context():
-		refresh_schedule()
+    """Initialize and start the scheduler with the Flask app context."""
+    scheduler.start()
+    with app.app_context():
+        refresh_schedule()
 
 def refresh_schedule():
-	"""Refresh the scheduler with the latest shows from the database."""
-	inspector = inspect(db.engine)
-	'show' in inspector.get_table_names()
-	scheduler.remove_all_jobs()
-	shows = Show.query.all()
-	for show in shows:
-		schedule_recording(show)
+    """Refresh the scheduler with the latest shows from the database."""
+    inspector = inspect(db.engine)
+    'show' in inspector.get_table_names()
+    scheduler.remove_all_jobs()
+    shows = Show.query.all()
+    for show in shows:
+        schedule_recording(show)
 
 def record_stream(STREAM_URL, duration, output_file):
-	"""Records the stream using FFmpeg."""
-	output_file = f"{output_file}_{datetime.now().strftime('%m-%d-%y')}_RAWDATA.mp3"
-	try:
-		(
-			ffmpeg
-			.input(STREAM_URL, t=duration)
-			.output(output_file, acodec='copy')
-			.overwrite_output()
-			.run()
-			)
-	except ffmpeg._run.Error as e:
-		current_app.logger.error(f"FFmpeg error: {e.stderr.decode()}")
+    """Records the stream using FFmpeg."""
+    output_file = f"{output_file}_{datetime.now().strftime('%m-%d-%y')}_RAWDATA.mp3"
+    try:
+        (
+            ffmpeg
+            .input(STREAM_URL, t=duration)
+            .output(output_file, acodec='copy')
+            .overwrite_output()
+            .run()
+        )
+    except ffmpeg._run.Error as e:
+        with current_app.app_context():
+            current_app.logger.error(f"FFmpeg error: {e.stderr.decode()}")
 
 def delete_show(show_id):
-	"""Delete a show from the database after its last airing."""
-	with db.app_context():
-		show = Show.query.get(show_id)
-		if show:
-			db.session.delete(show)
-			db.session.commit()
+    """Delete a show from the database after its last airing."""
+    with db.app_context():
+        show = Show.query.get(show_id)
+        if show:
+            db.session.delete(show)
+            db.session.commit()
 
 def schedule_recording(show):
     """Schedules the recurring recording and deletion of a show."""
